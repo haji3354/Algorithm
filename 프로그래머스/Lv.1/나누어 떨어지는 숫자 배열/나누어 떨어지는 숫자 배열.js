bfb4aa058a136eef0a1df0a1b5fa4a0e0d615a49
YeomHyeJi <haji3354@naver.com>
2022-10-29 18:22:04 +0900

    Lv 1 나누어 떨어지는 숫자 배열

---
+++ 프로그래머스/Lv.1/나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -0,0 +1,13 @@
+function solution(arr, divisor) {
+  let answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % divisor === 0) {
+      answer.push(arr[i]);
+    }
+  }
+  answer.sort((a, b) => a - b);
+  if (answer.length === 0) {
+    answer = [-1];
+  }
+  return answer;
+}
